577643939927c0cc07b2af762290406ad9fde831
Quentin Pleplé <quentin.pleple@gmail.com>
2013-02-08 17:55:30 -0800

    automatized

--- main.py
+++ main.py
@@ -31,18 +31,69 @@ def bowCorpus(root_path):
 
     return corpus, vocab
 
-corpus = {'20news': '/Users/qt/Desktop/research-data/20news/all'}
-for name, path in corpus.items():
-    print "Corpus", colored(name, 'green')
-    save_to = path + '-output'
-    if not os.path.exists(save_to):
-        os.makedirs(save_to)
-    corpus_path = os.path.join(save_to, 'corpus.pickle')
-    vocab_path  = os.path.join(save_to, 'corpus.vocab')
-    if os.path.exists(corpus_path):
-        print colored(corpus_path, 'green'), "already exists"
-        continue
-    corpus, vocab = bowCorpus(path)
-    dump(corpus, open(corpus_path, 'w'))
-    dump(vocab, open(vocab_path, 'w'))
-    
+def bowCorpuses(paths):
+    cprint("{0:.^80}".format(" Preprocessing "), 'yellow')
+    for name, path in paths.items():
+        print "Corpus", colored(name, 'green')
+        
+        save_to = path + '-output'
+        if not os.path.exists(save_to):
+            os.makedirs(save_to)
+        
+        corpus_path = os.path.join(save_to, 'corpus.pickle')
+        vocab_path  = os.path.join(save_to, 'vocab.pickle')
+        if os.path.exists(corpus_path):
+            print colored(corpus_path, 'green'), "already exists"
+            continue
+
+        corpus, vocab = bowCorpus(path)
+        dump(corpus, open(corpus_path, 'w'))
+        dump(vocab, open(vocab_path, 'w'))
+
+def bytesString(bytes):
+    power = int(math.log(bytes, 1000))
+    scaled = bytes / 1000.**power
+    prefixes = ' kMGT'
+    return '%.1f %sB' % (scaled, prefixes[power])
+
+
+def ldaCorpuses(paths):
+    cprint("{0:.^80}".format(" LDA "), 'yellow')
+    for name, path in paths.items():
+        print "Corpus", colored(name, 'green')
+        
+        save_to  = path + '-output'
+        lda_path = os.path.join(save_to, 'lda.pickle')
+
+        if os.path.exists(lda_path):
+            print colored(lda_path, 'green'), "already exists"
+            continue
+
+        corpus_path = os.path.join(save_to, 'corpus.pickle')
+        vocab_path  = os.path.join(save_to, 'vocab.pickle')
+
+        sz = bytesString(os.path.getsize(vocab_path))
+        print "Loading vocab", colored(sz, 'green')
+        vocab  = load(open(vocab_path, 'r'))
+        sz = bytesString(os.path.getsize(corpus_path))
+        print "Loading corpus", colored(sz, 'green')
+        corpus = load(open(corpus_path, 'r'))
+
+        lda = models.ldamodel.LdaModel(
+            corpus       = corpus,
+            id2word      = vocab,
+            num_topics   = 100,
+            update_every = 0,
+            passes       = 50
+        )
+
+        lda.save(lda_path)
+        
+
+
+paths = {
+    '20news': '/Users/qt/Desktop/research-data/20news/all',
+    'nips'  : '/Users/qt/Desktop/research-data/nips/all',
+}
+bowCorpuses(paths)
+ldaCorpuses(paths)
